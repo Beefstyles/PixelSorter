347031ca2a616a2bf02678509bb5415205c82f1d
Beefstyles <glenwillis@gmail.com>
2017-02-25 17:04:02 +0800

    Will adapt source code to c# and convert to wpf app.

---
+++ PixelSorterWPF/PixelSorterWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixelSorterWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainImageLoaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bmap = new BitmapImage();
+            bmap.BeginInit();
+            bmap.UriSource = new Uri("C:\\Users\\Yggdrasil\\Documents\\PixelSorter\\Space.jpg");
+            bmap.EndInit();
+
+            var image = sender as Image;
+            image.Source = bmap;
+
+        }
+    }
+}
